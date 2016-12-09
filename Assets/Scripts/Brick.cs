00a633817fac0e838bd50e0cd216e307cdd6e7a8
Adam Boerema <adamboerema@gmail.com>
2016-12-08 23:18:33 -0500

    adding block and collision logic

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public int maxHits = 1;
+	private int timesHit = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		timesHit++;
+	}
+}
